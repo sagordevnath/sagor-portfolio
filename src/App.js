f0c79b7a63d6870ce68648ed48df37a127edcb24
sagor devnath <ssagor64@gmail.com>
2022-06-09 14:00:27 +0600

    service section create

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import About from './About';
 import './App.css';
 import Home from './Home';
+import Services from './Services';
 
 function App() {
   return (
     <div className='App'>
       <Home></Home>
       <About></About>
+      <Services></Services>
     </div>
   );
 }
